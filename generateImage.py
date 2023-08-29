3d6d569c8b4a0666bb27f1edee53337242931dee
lulamae12 <tls.tommy11@gmail.com>
2023-08-29 17:06:47 -0400

    COMMIT working basic

--- generateImage.py
+++ generateImage.py
@@ -1,29 +1,40 @@
 import numpy as np
 from PIL import Image
-import random
+import random,time
+
+SEED = ""
 
 def generateArray(w,h):
     pixelcount = w * h
-    npByteArray = np.arange(pixelcount)
-    print(npByteArray)
+    npByteArray = np.zeros((w,h,3), dtype=np.uint8)
+    #print(npByteArray)
     for i in range(w):
        for j in range(h): 
-        np.put(npByteArray,[i,j],generateRandom(""))
+        npByteArray[i,j] = generateRandom()
         
         
-    np.reshape(npByteArray,(w,h))
-    print(npByteArray)
+    #npByteArray = np.reshape(npByteArray,(w,h))
+    #print(npByteArray)
     generateImage(npByteArray)
 
-def generateRandom(seed):
-    return np.random.randint(0,250,size=3)
-    
+def generateRandom():
+    pixelArray = np.zeros(shape=(1,3),dtype=np.uint8)
+    pixelArray[0][0] = np.random.randint(0,255)# R
+    pixelArray[0][1] = np.random.randint(0,255)# G
+    pixelArray[0][2] = np.random.randint(0,255)# B
+    return pixelArray
 def generateImage(array):
     img = Image.fromarray(array)
     img.save("image.png")
 
 def main():
+    
 
-    generateArray(512,512)
+    while True:
+        seed = int(random.random() * 1000000000)
+        np.random.seed(int(seed))
+        print(f"SEED: {int(seed)}")
+        generateArray(32,32)
+        time.sleep(0.3)
 
 main()
