7012b70e7b1211e4f73b0397ea807e883c338a42
lulamae12 <tls.tommy11@gmail.com>
2023-08-29 20:34:59 -0400

    commit

--- generateImage.py
+++ generateImage.py
@@ -10,7 +10,9 @@ def generateArray(w,h):
     #print(npByteArray)
     for i in range(w):
        for j in range(h): 
-        npByteArray[i,j] = generateRandom()
+        pix = generateRandom()
+        npByteArray[i,j] = pix
+
         
         
     #npByteArray = np.reshape(npByteArray,(w,h))
@@ -19,22 +21,31 @@ def generateArray(w,h):
 
 def generateRandom():
     pixelArray = np.zeros(shape=(1,3),dtype=np.uint8)
-    pixelArray[0][0] = np.random.randint(0,255)# R
-    pixelArray[0][1] = np.random.randint(0,255)# G
-    pixelArray[0][2] = np.random.randint(0,255)# B
+    pixelArray[0][0] = (np.random.randint(0,256))# R
+    pixelArray[0][1] = (np.random.randint(0,256)# G
+    pixelArray[0][2] = (np.random.randint(0,256)# B
     return pixelArray
+
+
+def randomPixel(min,max):
+
+
 def generateImage(array):
     img = Image.fromarray(array)
     img.save("image.png")
 
 def main():
     
+    width=input("Width (px): ")
+    height=input("Height(px): ")
 
     while True:
-        seed = int(random.random() * 1000000000)
+        print(f"\n\n{width}px X {height}px")
+        seed = int(random.random() * 1000000000) 
         np.random.seed(int(seed))
         print(f"SEED: {int(seed)}")
-        generateArray(32,32)
+        generateArray(int(height),int(width))
+        #input("---------------------------\nPress enter to regenerate. ")
         time.sleep(0.3)
 
 main()
